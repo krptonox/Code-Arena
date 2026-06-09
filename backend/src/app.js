f237996e9100df620cd93bde916f9ffbd39bfd38
krptonox <aryanyadav182007@gmail.com>
2026-06-09 22:33:21 +0530

    Set up backend environment and server; configure CORS and add API routes

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(',') || 'http://localhost:5173',
+    credentials:true,
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders:['Content-Type', 'Authorization']
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get("/", (req, res) =>{
+      res.send("Welcome to Home API");
+});
+
+app.get('/codearena',(req,res) => {
+    res.send("Welcome to the Code Arena API")
+})
+
+export default app;
