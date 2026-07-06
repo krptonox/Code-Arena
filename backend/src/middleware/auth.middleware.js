fb576b31b656532360348cb52e456f6a761ff746
krptonox <aryanyadav182007@gmail.com>
2026-07-07 03:10:36 +0530

    fix: correct casing in import statement and environment variable reference in JWT verification

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,4 @@
-import {User} from '../models/user.model.js';
+import { User } from '../Models/user.model.js';
 import { asyncHandler } from '../utils/async-handler.js';
 import { ApiError } from '../utils/api-error.js';
 import jwt from 'jsonwebtoken';
@@ -6,14 +6,17 @@ import jwt from 'jsonwebtoken';
 export const verifyJwt = asyncHandler(async (req,res,next) => {
     const token = req.cookies?.accessToken || req.header('Authorization')?.replace("Bearer ","");
 
+    console.log("Cookies:", req.cookies);
+    console.log("Authorization:", req.headers.authorization);
+
     if(!token){
         throw new ApiError(401, "Unauthorized request");
     }
 
     try{
-        const decodeToken = jwt.verify(token, Process.env.ACCESS_TOKEN_SECRET);
+        const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-        const user  = await User.findbyId(decodeToken?._id).select( "-password -createdAt -updatedAt -refreshToken -__v");
+        const user  = await User.findById(decodeToken?._id).select( "-password -createdAt -updatedAt -refreshToken -__v");
 
         if(!user){
             throw new ApiError(404, "User not found");
